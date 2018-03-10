e0a2fa9b48a3b943961c3bafd6c982e221d3b7ea
hwaterke <hwaterke@users.noreply.github.com>
2018-03-10 09:59:16 +0100

    Updated index file to export relevant parts of the library

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-export const VERSION = '0.0.1'
+// @flow
+import config from '../package.json'
+
+export const VERSION = config.version
+
+// Reducers
+export {
+  createActivityReducersForResources,
+} from './reducers/createActivityReducersForResources'
+export {createReducersForResources} from './reducers/createReducersForResources'
+
+// Selectors
+export {select} from './selectors/select'
+
+// Thunks
+export {createCrudThunks} from './thunks/createCrudThunks'
+
+// Types
+export type {ResourceDefinition} from './types/ResourceDefinition'
